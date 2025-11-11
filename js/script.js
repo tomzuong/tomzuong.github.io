2d8de812f42d56c07b8c8661a9b09b04f5860ace
tomzuong <vuthaiduong528@gmail.com>
2025-11-11 21:27:31 +0700

    Learn function/var/prompt/console.log on js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-var name = "Duong Tap Lap Trinh";
-console.log(name);
-console.log(name);
-console.log(name);
-console.log(name);
+function calculateAge(yearBorn) {
+	var currentTime = new Date();
+	var currentYear = currentTime.getFullYear();
+	return currentYear - yearBorn;
+}
+
+
+var name = prompt("Nhập tên của bạn:");
+
+var yearBorn = prompt("Nhập năm sinh của bạn:");
+
+var age = calculateAge(yearBorn);
+
+console.log(name + " is " + age + " years old");
